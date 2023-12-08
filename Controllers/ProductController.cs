26d8fb3ef80c032fdbf844e9ab1c666cd51003c2
okhmadjonov <73591790+okhmadjonov@users.noreply.github.com>
2023-12-08 18:00:50 +0500

    Update ProductController.cs
    Delete method added

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -45,18 +45,44 @@ namespace TestTime.Controllers
             return View("Update", roleProductDto);
         }
 
-        [HttpPost]
-        [Authorize(Roles = "ADMIN")]
-        public async Task<IActionResult> DeleteAsync(string userId, string userName, string role, string id)
-        {
-            if (!ModelState.IsValid) return View("Index");
+        // [HttpPost]
+        // [Authorize(Roles = "ADMIN")]
+        // public async Task<IActionResult> DeleteAsync(string userId, string userName, string role, string id)
+        // {
+        //     if (!ModelState.IsValid) return View("Index");
 
-            await _productService.Delete(userId, userName, id);
-            var roleProductDto = await _productService.RetrieveDto(userId, userName, role);
-            return View("Delete", roleProductDto);
+        //     await _productService.Delete(userId, userName, id);
+        //     var roleProductDto = await _productService.RetrieveDto(userId, userName, role);
+        //     return View("Delete", roleProductDto);
+        // }
+
+             // Delete task
+        [HttpGet]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var task = await _productService.Get(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
         }
 
-     
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.GetUserAsync(User);
+            var username = user!.UserName;
+            await _productService.Delete(id, userId!, username!);
+
+            return RedirectToAction("Index");
+        }
+
         [Authorize(Roles = "ADMIN")]
         [HttpGet] // Remove this line if you have it
         public async Task<ViewResult> Create()
