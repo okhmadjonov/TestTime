f5833839c2e4a5c318e89a8999e645b6749424ff
okhmadjonov <oakhmadjonov1@gmail.com>
2023-12-08 17:49:48 +0500

    Added product added

--- obj/Debug/net7.0/TestTime.AssemblyInfo.cs
+++ obj/Debug/net7.0/TestTime.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestTime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5c35ed45f9fb290a4a9c4eb88ffcd3ae6d8ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestTime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestTime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
